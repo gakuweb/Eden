5160d60c93548136f130619d09686b39f4214b20
Ryo NAKAMURA <r7kamura@gmail.com>
2010-09-13 19:35:05 +0900

    doshisha を含む単語を検索する単純なクローラー作成

--- cron/userinfor.rb
+++ cron/userinfor.rb
@@ -1,23 +1,45 @@
-require "rubygems"
-require "twitter"
 require "oauth"
+require "rubytter"
 
-=begin
-consumer_key="JQ1Pga5Gfy1VlXADwkNJEA"
-consumer_secret="j5irUbRJfS7kkeCSpFG2wE8iYZjppS7ax2tNINhc1U"
-access_token="151763436-x7sSNLfBoYdqSvYrfpFMlzLyuv28ojg2zGnmQmOv"
-access_secret="WsaNJZ1D6x1jM7whsKzDi5NupLQacXBsocUpnue2HQ"
-oauth = Twitter::OAuth.new(consumer_key, consumer_secret)
-oauth.authorize_from_access(access_token, access_secret)
-twitter = Twitter::Base.new(oauth)
-=end
-#twitter.update("ok")
-friends=Twitter.friend_ids("doshisha")		#doshishaがフォローしているユーザidを抽出
 
-1.upto(2) do |a|
-	tweet=Tweets.new
-	tweet=Twitter.user(friends[a])[:name]
-	tweet=Twitter.user(friends[a])[:profile_image_url]
-	tweet.save
+CONSUMER_KEY    = "3UKpPByx3WxnuKUByK53Yw"
+CONSUMER_SECRET = "BjKHnnAOSs8mrgfXbHbCqTJPq4Swom3q7BIIWG00Rs"
+ACCESS_TOKEN    = "31145622-R7rA5BNJNIvu1s4RiAeAKePkXdjijlIfdDJxuh1U7"
+ACCESS_SECRET   = "BOqUUymhB09XKI7R2NYdAhbRBSTjPdFcrhaaUuPM"
+
+def create_rubytter
+  consumer = OAuth::Consumer.new(
+    CONSUMER_KEY,
+    CONSUMER_SECRET,
+    :site => 'http://api.twitter.com'
+  )
+  token = OAuth::AccessToken.new(
+    consumer,
+    ACCESS_TOKEN,
+    ACCESS_SECRET
+  )
+  OAuthRubytter.new(token)
 end
 
+def save_tweets
+  # no use OAuth
+  # rubytter = create_rubytter
+  rubytter = Rubytter.new
+  tweets = rubytter.search("doshisha")
+
+  tweets.each do |t|
+    tweet = Tweet.new
+    tweet.text              = t.text
+    tweet.user              = t.user.screen_name
+    tweet.profile_image_url = t.user.profile_image_url
+    tweet.posted_at         = t.created_at
+    tweet.status_id         = t.id
+    if tweet.save
+      puts tweet.user
+    else
+      puts "failed."
+    end
+  end
+end
+
+save_tweets
