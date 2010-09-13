669999fd6ebacf6775e48f3fb4f21e1f70cce94e
Ryo NAKAMURA <r7kamura@gmail.com>
2010-09-13 15:44:05 +0900

    最初のコミット

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eden::Application.config.secret_token = '6c0c4a6ad489b9379d9443a9655ad2735bbf5aa69c655db0d4d7b2f2e8445e8a27fc3920d784dfd1539a9999739c540ec51a0dc6e57b8ccb2b9ae33fa72fcf10'
