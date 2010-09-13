aaac3eeca31a221794fd12fe0278fe5e85a136c2
Ryo NAKAMURA <r7kamura@gmail.com>
2010-09-13 17:25:04 +0900

    TweetModel追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   def index
+    @tweets = Tweet.find(:all, :order => 'posted_at DESC')
   end
 
 end
--- db/migrate/20100913065629_create_tweets.rb
+++ db/migrate/20100913065629_create_tweets.rb
@@ -1,7 +1,11 @@
 class CreateTweets < ActiveRecord::Migration
   def self.up
     create_table :tweets do |t|
-
+      t.string :user
+      t.string :text
+      t.datetime :posted_at
+      t.string :profile_image_url
+      t.integer :status_id
       t.timestamps
     end
   end
