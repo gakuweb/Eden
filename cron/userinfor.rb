e7ead1dc55b0b24f826926ded7623550da565dc9
suehiro <suehiroyoshitaka@hotmail.co.jp>
2010-09-14 13:48:08 +0900

    アカウント同志社の発言抽出と「同志社」を含むツイートの抽出完了

--- cron/userinfor.rb
+++ cron/userinfor.rb
@@ -1,31 +1,35 @@
 require "rubygems"
 require "twitter"
+require "rubytter"
 
-=begin
-require "oauth"
-consumer_key="JQ1Pga5Gfy1VlXADwkNJEA"
-consumer_secret="j5irUbRJfS7kkeCSpFG2wE8iYZjppS7ax2tNINhc1U"
-access_token="151763436-x7sSNLfBoYdqSvYrfpFMlzLyuv28ojg2zGnmQmOv"
-access_secret="WsaNJZ1D6x1jM7whsKzDi5NupLQacXBsocUpnue2HQ"
-oauth = Twitter::OAuth.new(consumer_key, consumer_secret)
-oauth.authorize_from_access(access_token, access_secret)
-twitter = Twitter::Base.new(oauth)
-=end
-
-friends=Twitter.friend_ids("doshisha")		#doshishaがフォローしているユーザidを抽出
-
-count=0
-#DB(Tweet)に”ユーザネーム”と”アイコンのurl”と”最新tweet”と”tweetのid”を保存する
-friends.each do |person|
-	tweet=Tweet.new
-	user_infor=Twitter.user(person)
-	tweet.user=user_infor[:name]
-	tweet.profile_image_url=user_infor[:profile_image_url]
-	tweet.text=user_infor[:status][:text]
-	tweet.status_id=user_infor[:status][:id]
-	tweet.save
-	count=count+1
-	if count==10
-		break
+#アカウント同志社が発言したものを抽出
+def search_doshisha_tweet
+	timeline=Twitter.timeline("doshisha")		#doshishaのタイムラインを取得する
+	timeline.each do |doshisha|
+		tweet=Tweet.new
+		tweet.text= doshisha[:text]
+		tweet.status_id= doshisha[:id]
+		tweet.user=doshisha[:user][:screen_name]
+		tweet.profile_image_url=doshisha[:user][:profile_image_url]
+		tweet.posted_at=doshisha[:created_at]
+		tweet.save
 	end
 end
+
+#同志社を含むツイートを抽出
+def search_word_doshisha
+	rubytter=Rubytter.new
+	tweets=rubytter.search("同志社")
+	tweets.each do |person|
+		tweet = Tweet.new	
+		tweet.text = person.text
+		tweet.user = person.user.screen_name
+		tweet.profile_image_url = person.user.profile_image_url 		
+		tweet.posted_at= person.created_at
+	    tweet.status_id = person.id
+		tweet.save
+	end
+end
+
+search_doshisha_tweet
+search_word_doshisha
