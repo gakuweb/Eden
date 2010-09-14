2f6261ac23ee71c3915f7455a50216bc81936cc7
suehiro <suehiroyoshitaka@hotmail.co.jp>
2010-09-14 09:38:49 +0900

    同志社がフォローしているユーザの情報抽出作成

--- cron/userinfor.rb
+++ cron/userinfor.rb
@@ -1,8 +1,8 @@
 require "rubygems"
 require "twitter"
-require "oauth"
 
 =begin
+require "oauth"
 consumer_key="JQ1Pga5Gfy1VlXADwkNJEA"
 consumer_secret="j5irUbRJfS7kkeCSpFG2wE8iYZjppS7ax2tNINhc1U"
 access_token="151763436-x7sSNLfBoYdqSvYrfpFMlzLyuv28ojg2zGnmQmOv"
@@ -11,13 +11,21 @@ oauth = Twitter::OAuth.new(consumer_key, consumer_secret)
 oauth.authorize_from_access(access_token, access_secret)
 twitter = Twitter::Base.new(oauth)
 =end
-#twitter.update("ok")
+
 friends=Twitter.friend_ids("doshisha")		#doshishaがフォローしているユーザidを抽出
 
-1.upto(2) do |a|
-	tweet=Tweets.new
-	tweet=Twitter.user(friends[a])[:name]
-	tweet=Twitter.user(friends[a])[:profile_image_url]
+count=0
+#DB(Tweet)に”ユーザネーム”と”アイコンのurl”と”最新tweet”と”tweetのid”を保存する
+friends.each do |person|
+	tweet=Tweet.new
+	user_infor=Twitter.user(person)
+	tweet.user=user_infor[:name]
+	tweet.profile_image_url=user_infor[:profile_image_url]
+	tweet.text=user_infor[:status][:text]
+	tweet.status_id=user_infor[:status][:id]
 	tweet.save
+	count=count+1
+	if count==10
+		break
+	end
 end
-
